21cc5a3f1869f19a8fadf35fce1010b1a6a24683
tatendabakozw <tatendabakozw@gmail.com>
2022-03-23 09:28:41 +0200

    fix: dashboard inventory mobile layout

--- pages/dashboard/inventory/index.tsx
+++ pages/dashboard/inventory/index.tsx
@@ -38,11 +38,11 @@ export default function Inventory() {
 
     return (
         <DashboardLayout>
-            <div className="flex flex-col p-8">
-                <div className="flex bg-white rounded shadow p-4 flex-col mb-8">
-                    <div className="flex flex-row items-center space-x-8">
-                        <p className="text text-gray-700 font-semibold">Products</p>
-                        <div className="flex flex-row items-center border flex-1 border-gray-200 rounded px-2 text-gray-500">
+            <div className="flex flex-col md:p-8 p-2">
+                <div className="flex bg-white rounded shadow md:p-4 p-2 flex-col mb-8">
+                    <div className="flex md:flex-row flex-col items-center md:space-x-8 space-x-2">
+                        {/* <p className="text text-gray-700 font-semibold">Products</p> */}
+                        <div className="flex flex-row items-center border flex-1 w-full md:mb-0 mb-4 border-gray-200 rounded px-2 text-gray-500">
                             <SearchIcon height={20} width={20} />
                             <input 
                                 type="text" 
@@ -50,10 +50,7 @@ export default function Inventory() {
                                 placeholder="Search something and press enter" className="p-2 flex-1 outline-none" />
                         </div>
                         <BlueButton text="Create Product" onClick={() => history.push('/dashboard/inventory/create')} />
-                        <div onClick={() => setShowFilter(true ? false : true)} className="flex cursor-pointer flex-row items-center space-x-2 text-blue-primary">
-                            <p>Filter</p>
-                            <ArrowDownIcon height={16} width={16} />
-                        </div>
+                       
                     </div>
                 </div>
 
