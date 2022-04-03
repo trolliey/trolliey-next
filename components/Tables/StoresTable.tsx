8915cd80aa39f2b2f7c32d2cca13dda8bff921b1
tatendabakozw <tatendabakozw@gmail.com>
2022-04-03 13:51:20 +0200

    feat: manage stores table

---
+++ components/Tables/StoresTable.tsx
@@ -0,0 +1,78 @@
+import { Avatar } from '@chakra-ui/avatar'
+import { SearchIcon } from '@heroicons/react/outline'
+import React, { useState } from 'react'
+import moment from 'moment'
+import BlueButton from '../Buttons/BlueButton'
+import StoresDropdown from '../Dropdowns/StoresDropdown'
+
+interface Props{
+    stores?:any
+}
+
+function StoresTable({ stores }:Props) {
+    const [search_query, setSearchQuery] = useState<any>('')
+
+    const search_item = () => {
+        console.log(search_query)
+    }
+    return (
+        <div className="flex flex-col mx-4">
+            <div className="flex flex-row items-center space-x-2 w-full mb-4 ">
+                <div className="flex flex-row items-center border border-gray-200 rounded px-4 py-1 flex-1">
+                    <SearchIcon height={16} width={16} className="text-gray-300" />
+                    <input
+                        type="text"
+                        placeholder="Search user by name or id"
+                        onChange={e => setSearchQuery(e.target.value)}
+                        className="p-2 bg-gray-100 flex-1 outline-none border-none" />
+                </div>
+                <BlueButton text="Search" onClick={search_item} />
+            </div>
+            <div className="grid grid-cols-6 items-center rounded-t shadow border-b border-gray-100">
+                <div className="col-span-1 text-gray-400 uppercase p-2 bg-gray-50  ">
+                    logo
+                </div>
+                <div className="col-span-2 text-gray-400 uppercase p-2 bg-gray-50 ">
+                    email
+                </div>
+                <div className="col-span-1 text-gray-400 uppercase p-2 bg-gray-50 ">
+                    verified
+                </div>
+                <div className="col-span-1 text-gray-400 uppercase p-2 bg-gray-50 ">
+                    Created
+                </div>
+                <div className="col-span-1 text-gray-400 uppercase p-2 bg-gray-50 ">
+                    edit
+                </div>
+            </div>
+
+            {
+                stores?.map((store:any, index:number) => (
+                    <div key={index} className="grid grid-cols-6 w-full bg-white p-2 flex-1 items-center rounded-t shadow border-b text-gray-800 border-gray-100 ">
+                        <div className="col-span-1 flex flex-row items-center gap-2  ">
+                            <Avatar src={store?.photoURL} name={store?.company_name} />
+                            <p>
+                                {store?.company_name}
+                            </p>
+                        </div>
+                        <div className="col-span-2  ">
+                            {store?.email}
+                        </div>
+                        <div className="col-span-1  ">
+                            {store?.verified ? 'true' : 'false'}
+                        </div>
+                        <div className="col-span-1  ">
+                            {moment(store?.createdAt).fromNow()}
+                        </div>
+                        <div className="col-span-1  text-blue-primary ">
+                            <StoresDropdown id={store._id} />
+                        </div>
+                    </div>
+                ))
+            }
+
+        </div>
+    )
+}
+
+export default StoresTable
--- pages/admin/stores/index.tsx
+++ pages/admin/stores/index.tsx
@@ -1,12 +1,34 @@
 import React from 'react'
+import StoresTable from '../../../components/Tables/StoresTable'
 import AdminDashboard from '../../../layouts/AdminDashboard'
+import Store from '../../../models/Store'
+import { connect, disconnect } from '../../../utils/mongo'
 
-function ManageStores() {
-    return (
-        <AdminDashboard>
-            manage all stores from here
-        </AdminDashboard>
-    )
+function ManageStores(props: any) {
+  //   const stores: any = []
+  const { stores} = props
+  console.log(stores)
+  return (
+    <AdminDashboard>
+      <p className="my-4 text-center text-lg font-semibold capitalize text-gray-700">
+        manage all stores
+      </p>
+      <div className="min-h-screen">
+        <StoresTable stores={stores} />
+      </div>
+    </AdminDashboard>
+  )
+}
+
+export async function getServerSideProps(context: any) {
+  await connect()
+  const stores = await Store.find({}).lean()
+  await disconnect()
+  return {
+    props: {
+      stores: JSON.parse(JSON.stringify(stores)),
+    },
+  }
 }
 
 export default ManageStores
