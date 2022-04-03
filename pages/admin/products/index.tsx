adc19476625b1f476ac3e6a1e456ac769434c549
tatendabakozw <tatendabakozw@gmail.com>
2022-04-03 15:36:21 +0200

    feat: admin products table

--- pages/admin/products/index.tsx
+++ pages/admin/products/index.tsx
@@ -1,12 +1,34 @@
 import React from 'react'
+import ProductsTable from '../../../components/Tables/ProductsTable'
 import AdminDashboard from '../../../layouts/AdminDashboard'
+import Products from '../../../models/Product'
+import { connect, disconnect } from '../../../utils/mongo'
 
-function ManageProducts() {
+function ManageProducts(props: any) {
+    const { products } = props
+    console.log(products)
     return (
         <AdminDashboard>
-            manage all products from here
+             <div className="flex flex-col w-full flex-1 px-4">
+                <p className="text-gray-900 font-semibold text-center my-4">Manage all products</p>
+                <div className="flex flex-col">
+                    <ProductsTable 
+                        products={products} />
+                </div>
+            </div>
         </AdminDashboard>
     )
 }
+
+export async function getServerSideProps(context: any) {
+    await connect()
+    const products = await Products.find({}).lean()
+    await disconnect()
+    return {
+      props: {
+        products: JSON.parse(JSON.stringify(products)),
+      },
+    }
+  }
 
 export default ManageProducts
