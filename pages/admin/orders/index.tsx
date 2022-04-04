c7ee331bec8d4795e929e4e4eb254d5d7442bfb1
tatendabakozw <tatendabakozw@gmail.com>
2022-04-04 13:23:04 +0200

    feat: added orders table

--- pages/admin/orders/index.tsx
+++ pages/admin/orders/index.tsx
@@ -1,12 +1,54 @@
 import React from 'react'
 import AdminDashboard from '../../../layouts/AdminDashboard'
+import Orders from '../../../models/Order'
+import { connect, disconnect } from '../../../utils/mongo'
+import moment from 'moment'
 
-function ManageOrders() {
-    return (
-        <AdminDashboard>
-            manage all orders
-        </AdminDashboard>
-    )
+function ManageOrders(props: any) {
+  const { orders } = props
+
+  console.log(orders)
+
+  return (
+    <AdminDashboard>
+      <p className="my-8 text-center text-lg font-semibold text-gray-800">
+        Manage all orders from here!
+      </p>
+      <div className="flex flex-col px-2 md:px-4">
+        <div className="grid grid-cols-5 rounded-t bg-black p-2 text-sm text-white">
+          <div className="col-span-1">order number</div>
+          <div className="col-span-1">status</div>
+          <div className="col-span-1">Orderd by</div>
+          <div className="col-span-1">time orderd</div>
+          <div className="col-span-1">action</div>
+        </div>
+        {orders?.map((order: any, index: number) => (
+          <div className="grid grid-cols-5 p-2">
+            <div className="col-span-1">{order._id}</div>
+            <div className="col-span-1">
+              {order.isDelivered ? 'delivered' : 'pending'}
+            </div>
+            <div className="col-span-1">{order.full_name}</div>
+            <div className="col-span-1">
+              {moment(order.createdAt).fromNow()}
+            </div>
+            <div className="col-span-1">action</div>
+          </div>
+        ))}
+      </div>
+    </AdminDashboard>
+  )
+}
+
+export async function getServerSideProps(context: any) {
+  await connect()
+  const orders = await Orders.find({}).lean()
+  await disconnect()
+  return {
+    props: {
+      orders: JSON.parse(JSON.stringify(orders)),
+    },
+  }
 }
 
 export default ManageOrders
--- pages/admin/products/index.tsx
+++ pages/admin/products/index.tsx
@@ -5,30 +5,31 @@ import Products from '../../../models/Product'
 import { connect, disconnect } from '../../../utils/mongo'
 
 function ManageProducts(props: any) {
-    const { products } = props
-    console.log(products)
-    return (
-        <AdminDashboard>
-             <div className="flex flex-col w-full flex-1 px-4">
-                <p className="text-gray-900 font-semibold text-center my-4">Manage all products</p>
-                <div className="flex flex-col">
-                    <ProductsTable 
-                        products={products} />
-                </div>
-            </div>
-        </AdminDashboard>
-    )
+  const { products } = props
+  console.log(products)
+  return (
+    <AdminDashboard>
+      <div className="flex w-full flex-1 flex-col px-4">
+        <p className="my-4 text-center font-semibold text-gray-900">
+          Manage all products
+        </p>
+        <div className="flex flex-col">
+          <ProductsTable products={products} />
+        </div>
+      </div>
+    </AdminDashboard>
+  )
 }
 
 export async function getServerSideProps(context: any) {
-    await connect()
-    const products = await Products.find({}).lean()
-    await disconnect()
-    return {
-      props: {
-        products: JSON.parse(JSON.stringify(products)),
-      },
-    }
+  await connect()
+  const products = await Products.find({}).lean()
+  await disconnect()
+  return {
+    props: {
+      products: JSON.parse(JSON.stringify(products)),
+    },
   }
+}
 
 export default ManageProducts
