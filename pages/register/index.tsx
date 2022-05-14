c42c599b227ed01329918578d595c103bd5db35f
tatendabakozw <tatendabakozw@gmail.com>
2022-05-14 23:18:51 +0200

    feat: currency items

--- pages/register/index.tsx
+++ pages/register/index.tsx
@@ -130,7 +130,7 @@ function Register() {
                                         onChange={e => setAgreed(e.target.checked)}
                                         className="h-4 w-4 text-blue-primary focus:ring-red-400 border-gray-300 rounded"
                                     />
-                                    <label htmlFor="terms" className="ml-2 block text-sm text-gray-900">
+                                    <label onClick={()=> history.push('/termsandconditions')} htmlFor="terms" className="ml-2 block text-sm text-gray-900">
                                         I agree to the terms and conditions
                                     </label>
                                 </div>
