d7c741ed4e3c067e172a3b470e0e1352f3a0b04e
tatendabakozw <tatendabakozw@gmail.com>
2022-05-19 13:54:29 +0200

    fix: description page

--- pages/categories/index.tsx
+++ pages/categories/index.tsx
@@ -6,42 +6,56 @@ import GeneralLayout from '../../layouts/GeneralLayout'
 import { data } from '../../utils/data'
 
 function Categories() {
-    const { dispatch } = useContext(Store)
-    const history = useRouter()
+  const { dispatch } = useContext(Store)
+  const history = useRouter()
 
-    const search_by_category = (category: any) => {
-        dispatch({ type: 'SET_SEARCH_QUERY', payload: category })
-        history.push('/explore')
-    }
-    return (
-        <GeneralLayout title="Explore Products" description="Explore all all products offered by Trolliey marketplace">
-            <div className="flex flex-col flex-wrap items-center max-w-7xl mb-8">
-                <div className="grid md:grid-cols-3 grid-cols-2 md:gap-8 gap-4 mx-auto max-w-7xl">
-                    {data.categories.map((category, index) => (
-                        <div onClick={()=>search_by_category(category.name)}  key={index} className="transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:transform-none cursor-pointer hover:text-blue-primary col-span-1">
-                            <CategoryItem text={category.name} image={category.icon} />
-                        </div>
-                    ))}
-                </div>
+  const search_by_category = (category: any) => {
+    dispatch({ type: 'SET_SEARCH_QUERY', payload: category })
+    history.push('/explore')
+  }
+  return (
+    <GeneralLayout
+    no_text
+      title="Explore Products"
+      description="Explore all all products offered by Trolliey marketplace"
+    >
+      <div className="mb-8 flex md:my-16 my-8 max-w-7xl flex-col flex-wrap items-center">
+        <div className="mx-auto grid max-w-7xl grid-cols-2 gap-4 md:grid-cols-3 md:gap-8">
+          {data.categories.map((category, index) => (
+            <div
+              onClick={() => search_by_category(category.name)}
+              key={index}
+              className="col-span-1 transform cursor-pointer transition hover:-translate-y-1 hover:text-blue-primary motion-reduce:transform-none motion-reduce:transition-none"
+            >
+              <CategoryItem text={category.name} image={category.icon} />
             </div>
-        </GeneralLayout>
-    )
-}
-
-interface CatProps{
-    text: string,
-    image: any
-}
-
-const CategoryItem = ({ text, image }:CatProps) => {
-    return (
-        <div className="bg-white p-2 rounded m-1 grid items-center justify-center content-center shadow w-full">
-            <div className='p-4 grid items-center justify-center content-center'>
-            <Image height={150} objectFit="contain" src={image} alt="" className='h-24' />
-            </div>
-            <p className='text-center'>{text}</p>
+          ))}
         </div>
-    )
+      </div>
+    </GeneralLayout>
+  )
+}
+
+interface CatProps {
+  text: string
+  image: any
+}
+
+const CategoryItem = ({ text, image }: CatProps) => {
+  return (
+    <div className="m-1 grid w-full content-center items-center justify-center rounded bg-white p-2 shadow">
+      <div className="relative h-48 w-48 content-center items-center justify-center p-4">
+        <Image
+          layout="fill"
+          objectFit="contain"
+          src={image}
+          alt=""
+          className="h-24"
+        />
+      </div>
+      <p className="text-center">{text}</p>
+    </div>
+  )
 }
 
 export default Categories
