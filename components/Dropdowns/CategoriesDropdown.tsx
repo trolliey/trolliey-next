1e14f27d20904e476ab3171f02e15e433e8ecc34
tatendabakozw <tatendabakozw@gmail.com>
2022-05-22 22:26:00 +0200

    fix: items category and sub categories searcy fueature

--- components/Dropdowns/CategoriesDropdown.tsx
+++ components/Dropdowns/CategoriesDropdown.tsx
@@ -1,66 +1,84 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react'
 import { ChevronRightIcon } from '@heroicons/react/outline'
-import { ChevronDownIcon } from '@heroicons/react/solid';
+import { ChevronDownIcon } from '@heroicons/react/solid'
 import SubCategoryComponent from './SubCategoryComponent'
-import { data } from '../../utils/data';
-import slugify from '../../utils/slugify';
-import { useRouter } from 'next/router';
+import { data } from '../../utils/data'
+import slugify from '../../utils/slugify'
+import { useRouter } from 'next/router'
+import { Store } from '../../Context/Store'
 
 function CategoriesDropdown() {
-    const history = useRouter()
-    const [category_slug, setCategorySlug] = useState('')
-    const [cat_name, setCatName] = useState('')
-    const [category_image, setCategoryImage] = useState()
+  const history = useRouter()
+  const [category_slug, setCategorySlug] = useState('')
+  const [cat_name, setCatName] = useState('')
+  const [category_image, setCategoryImage] = useState()
 
-    const handle_hover = (slug:any, name:any, imag:any) => {
-        setCategorySlug(slug)
-        setCatName(name)
-        setCategoryImage(imag)
+  const { dispatch } = useContext(Store)
 
-    }
+  const handle_hover = (slug: any, name: any, imag: any) => {
+    setCategorySlug(slug)
+    setCatName(name)
+    setCategoryImage(imag)
+  }
 
-    return (
-        <div className='lg:w-60 md:w-96'>
-            <ul className="menu relative text-gray-700 font-semibold">
-                <div className="flex flex-row items-center gap-8 bg-blue-primary text-white p-3 justify-between capitalize text-sm rounded-t">
-                    <p className='pl-2 pr-8 lg:flex md:hidden hidden'>By Category</p>
-                    <p className='pl-2 pr-8 lg:hidden md:flex hidden'>By Category</p>
-                    <ChevronDownIcon height={16} width={16} />
-                </div>
-                <li className='border border-gray-200 rounded-b'>
-                    <>
-                        {
-                            data.categories.slice(0, 10)?.map((category, index) => (
-                                <div
-                                    key={index}
-                                    onClick={() => history.push(`/sub-category/${slugify(category.name)}`)}
-                                    onMouseEnter={() => handle_hover(slugify(category.name), category.name, category.icon)}
-                                    className="flex flex-row items-center gap-2 py-2 px-4 cursor-pointer justify-between text-sm hover:bg-gray-100 overflow-ellipsis overflow-hidden">
-                                    <p className='capitalize overflow-ellipsis line-clamp-1'>{category.name}</p>
-                                    <ChevronRightIcon height={16} width={16} className='text-gray-400' />
-                                </div>
-                            ))
-                        }
-                    </>
-
-                    {
-                        category_slug && <>
-                            <SubCategoryComponent 
-                                category_id={category_slug} 
-                                cat_name={cat_name} 
-                                cat_image={category_image} />
-                        </>
-                    }
-
-                </li>
-                <div onClick={() => history.push('/categories')} className="flex flex-row items-center gap-2 py-2 px-4 border border-gra-200 rounded cursor-pointer justify-between text-sm hover:bg-gray-100">
-                    <p className='capitalize'>all categories</p>
-                    <ChevronRightIcon height={16} width={16} className='text-gray-400' />
-                </div>
-
-            </ul>
+  return (
+    <div className="md:w-96 lg:w-60">
+      <ul className="menu relative font-semibold text-gray-700">
+        <div className="flex flex-row items-center justify-between gap-8 rounded-t bg-blue-primary p-3 text-sm capitalize text-white">
+          <p className="hidden pl-2 pr-8 md:hidden lg:flex">By Category</p>
+          <p className="hidden pl-2 pr-8 md:flex lg:hidden">By Category</p>
+          <ChevronDownIcon height={16} width={16} />
         </div>
-    )
+        <li className="rounded-b border border-gray-200">
+          <>
+            {data.categories.slice(0, 10)?.map((category, index) => (
+              <div
+                key={index}
+                onClick={() => {
+                  dispatch({ type: 'SET_SEARCH_QUERY', payload: category })
+                  history.push('/explore')
+                }}
+                onMouseEnter={() =>
+                  handle_hover(
+                    slugify(category.name),
+                    category.name,
+                    category.icon
+                  )
+                }
+                className="flex cursor-pointer flex-row items-center justify-between gap-2 overflow-hidden overflow-ellipsis py-2 px-4 text-sm hover:bg-gray-100"
+              >
+                <p className="line-clamp-1 overflow-ellipsis capitalize">
+                  {category.name}
+                </p>
+                <ChevronRightIcon
+                  height={16}
+                  width={16}
+                  className="text-gray-400"
+                />
+              </div>
+            ))}
+          </>
+
+          {category_slug && (
+            <>
+              <SubCategoryComponent
+                category_id={category_slug}
+                cat_name={cat_name}
+                cat_image={category_image}
+              />
+            </>
+          )}
+        </li>
+        <div
+          onClick={() => history.push('/categories')}
+          className="border-gra-200 flex cursor-pointer flex-row items-center justify-between gap-2 rounded border py-2 px-4 text-sm hover:bg-gray-100"
+        >
+          <p className="capitalize">all categories</p>
+          <ChevronRightIcon height={16} width={16} className="text-gray-400" />
+        </div>
+      </ul>
+    </div>
+  )
 }
 
-export default CategoriesDropdown;
+export default CategoriesDropdown
--- components/Dropdowns/SubCategoryComponent.tsx
+++ components/Dropdowns/SubCategoryComponent.tsx
@@ -1,54 +1,71 @@
-import React from 'react';
-import useSWR from "swr";
-import { useRouter } from 'next/router';
-import Image from 'next/image';
+import React, { useContext } from 'react'
+import useSWR from 'swr'
+import { useRouter } from 'next/router'
+import Image from 'next/image'
+import { Store } from '../../Context/Store'
 
-interface Props{
-    category_id: string,
-    cat_name:string,
-    cat_image:any
+interface Props {
+  category_id: string
+  cat_name: string
+  cat_image: any
 }
 
-function SubCategoryComponent({category_id, cat_name, cat_image}:Props) {
-    const history = useRouter()
-    const { data: sub_categories, error: sub_cat_error } = useSWR(`/api/sub_category/all/${category_id}`)
+function SubCategoryComponent({ category_id, cat_name, cat_image }: Props) {
+  const history = useRouter()
+  const { data: sub_categories, error: sub_cat_error } = useSWR(
+    `/api/sub_category/all/${category_id}`
+  )
 
-    const search_handler = (search_query:string) => {
-        history.push('/explore')
-    }
-    return (
-        <div className="megadrop bg-gray-50 border border-gray-200 rounded flex flex-row z-10 shadow-lg">
-            <div className="w-3/5 ">
-                <p className='text-blue-primary font-semibold items-center text-lg p-4 capitalize'>{cat_name}</p>
-                <div className=" px-4">
-                    <ul className='bg-gray-50'>
-                        {
-                            !sub_categories ? (
-                                <p>loading...</p>
-                            ) : sub_cat_error ? (
-                                <p>error</p>
-                            ) : sub_categories?.sub_categories.length < 1 ? (
-                                <p className='text-center'>No subcategories to show</p>
-                            ) : (
-                                            <>
-                                                {
-                                                    sub_categories?.sub_categories.map((sub_cat:any, index:number) => (
-                                                        <li onClick={() => search_handler(sub_cat.sub_category)} key={index} className='bg-gray-50 cursor-pointer hover:text-black hover:font-semibold text-sm p-1 rounded hover:bg-gray-200'>
-                                                            <p className='text-gray-700 hover:text-black font-normal hover:font-semibold'>{sub_cat.sub_category}</p>
-                                                        </li>
-                                                    ))
-                                                }
-                                            </>
-                                        )
-                        }
-                    </ul>
-                </div>
-            </div>
-            <div className="relative w-2/5 bg-white grid items-center justify-center content-center">
-                <Image layout="fill" src={cat_image} className='h-32' alt="category representation on category component" />
-            </div>
+  const { dispatch } = useContext(Store)
+
+  const search_handler = (search_query: string) => {
+    dispatch({ type: 'SET_SEARCH_QUERY', payload: search_query })
+    history.push('/explore')
+  }
+  return (
+    <div className="megadrop z-10 flex flex-row rounded border border-gray-200 bg-gray-50 shadow-lg">
+      <div className="w-3/5 ">
+        <p className="items-center p-4 text-lg font-semibold capitalize text-blue-primary">
+          {cat_name}
+        </p>
+        <div className=" px-4">
+          <ul className="bg-gray-50">
+            {!sub_categories ? (
+              <p>loading...</p>
+            ) : sub_cat_error ? (
+              <p>error</p>
+            ) : sub_categories?.sub_categories.length < 1 ? (
+              <p className="text-center">No subcategories to show</p>
+            ) : (
+              <>
+                {sub_categories?.sub_categories.map(
+                  (sub_cat: any, index: number) => (
+                    <li
+                      onClick={() => search_handler(sub_cat.sub_category)}
+                      key={index}
+                      className="cursor-pointer rounded bg-gray-50 p-1 text-sm hover:bg-gray-200 hover:font-semibold hover:text-black"
+                    >
+                      <p className="font-normal text-gray-700 hover:font-semibold hover:text-black">
+                        {sub_cat.sub_category}
+                      </p>
+                    </li>
+                  )
+                )}
+              </>
+            )}
+          </ul>
         </div>
-    )
+      </div>
+      <div className="relative grid w-2/5 content-center items-center justify-center bg-white">
+        <Image
+          layout="fill"
+          src={cat_image}
+          className="h-32"
+          alt="category representation on category component"
+        />
+      </div>
+    </div>
+  )
 }
 
 export default SubCategoryComponent
