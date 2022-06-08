0886887e4261a852142fbbdc29fc8a9428555b19
tatendabakozw <tatendabakozw@gmail.com>
2022-06-08 05:37:10 +0200

    explore page search input

--- components/SearchInput/SearchInput.tsx
+++ components/SearchInput/SearchInput.tsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router'
 import BlueButton from '../Buttons/BlueButton'
 import slugify from '../../utils/slugify'
 import { Store } from '../../Context/Store'
+import { Input, Select } from '@chakra-ui/react'
 
 function SearchInput():ReactElement {
     const [search_query, setSearchQuery] = useState('')
@@ -21,20 +22,22 @@ function SearchInput():ReactElement {
     },[search_category])
 
     return (
-        <div className="flex flex-row items-center ">
-            <div className="flex flex-row search_and_dropdown border items-center border-gray-300 w-full rounded mr-2 bg-white">
-                <select
+        <div className="flex flex-row items-center pb-4 ">
+            <div className="flex flex-row search_and_dropdown space-x-2 items-center w-full rounded mr-2 bg-white">
+                <div className="flex">
+                <Select
                     onChange={e => setSearchCategory(e.target.value)}
                     name="categories"
                     id="categories"
-                    className="md:block hidden outline-none border-none md:p-3 p-2 mr-3 text-xs font-semibold text-gray-700 rounded">
+                    className="md:block hidden outline-none border-none text-xs font-semibold text-gray-700 rounded">
 
                     {data?.categories.map((category, index) => (
                         <option key={index} value={slugify(category.value)}>{category.name}</option>
                     ))}
 
-                </select>
-                <input
+                </Select>
+                </div>
+                <Input
                     type="text"
                     placeholder="Search Product Here..."
                     className="md:p-3 p-2 text-xs rounded-r border-l border-gray-200 flex-1 outline-none"
--- pages/explore/index.tsx
+++ pages/explore/index.tsx
@@ -7,6 +7,7 @@ import axios from 'axios'
 import no_product from '../../public/img/no_product.svg'
 import Image from 'next/image'
 import ProductItem from '../../components/ProductItem/ProductItem'
+import ProductLoading from '../../components/ProductItem/ProductLoading'
 
 export default function Explore(props: any) {
   // const { products } = props
@@ -36,54 +37,69 @@ export default function Explore(props: any) {
   return (
     <ExploreLayout>
       {
-        products?.length < 1 ? (
-          <div className=" h-96 grid items-center content-center justify-center">
-            <div className="relative h-40">
-              <Image src={no_product} layout="fill" objectFit="contain" />
-            </div>
-            <p className="text-center mt-4 capitalize font-semibold text-gray-700">no products found</p>
-          </div>
-        ) : (
-          <>
-          <div
-            className={`grid-cols-2 md:grid-cols-4 lg:grid-cols-4 mx-auto grid w-full gap-4 rounded-lg  md:gap-8`}
-          >
-            {products?.map((product: any, index: number) => (
+        loading ? (
+          <div className="scrollbar-hide relative mx-auto grid grid-cols-2 gap-4 rounded-lg bg-white p-4 md:grid-cols-3 md:gap-8 lg:grid-cols-4">
+            {[1, 2, 3, 4, 5]?.map((product: any, index: number) => (
               <div key={index} className="col-span-1 p-0">
-                {product.currency_type === currency ? (
-                  <ProductItem
-                    name={product.title}
-                    description={product.description}
-                    rating={product.rating}
-                    picture={product.pictures[0]}
-                    price={product.price}
-                    discount_price={product.discount_price}
-                    category={product.category}
-                    id={product._id}
-                    countInStock={product.countInStock}
-                    product={product}
-                    averageRating={product.averageRating}
-                  />
-                ) : currency === 'ANY' ? (
-                  <ProductItem
-                    name={product.title}
-                    description={product.description}
-                    rating={product.rating}
-                    picture={product.pictures[0]}
-                    price={product.price}
-                    discount_price={product.discount_price}
-                    category={product.category}
-                    id={product._id}
-                    countInStock={product.countInStock}
-                    product={product}
-                    averageRating={product.averageRating}
-                  />
-                ) : null}
+                <ProductLoading />
               </div>
             ))}
           </div>
-        </>
-          )
+        ) : (
+          <>
+            {
+              products?.length < 1 ? (
+                <div className=" h-96 grid items-center content-center justify-center">
+                  <div className="relative h-40">
+                    <Image src={no_product} layout="fill" objectFit="contain" />
+                  </div>
+                  <p className="text-center mt-4 capitalize font-semibold text-gray-700">no products found</p>
+                </div>
+              ) : (
+                <>
+                <div
+                  className={`grid-cols-2 md:grid-cols-4 lg:grid-cols-4 mx-auto grid w-full gap-4 rounded-lg  md:gap-8`}
+                >
+                  {products?.map((product: any, index: number) => (
+                    <div key={index} className="col-span-1 p-0">
+                      {product.currency_type === currency ? (
+                        <ProductItem
+                          name={product.title}
+                          description={product.description}
+                          rating={product.rating}
+                          picture={product.pictures[0]}
+                          price={product.price}
+                          discount_price={product.discount_price}
+                          category={product.category}
+                          id={product._id}
+                          countInStock={product.countInStock}
+                          product={product}
+                          averageRating={product.averageRating}
+                        />
+                      ) : currency === 'ANY' ? (
+                        <ProductItem
+                          name={product.title}
+                          description={product.description}
+                          rating={product.rating}
+                          picture={product.pictures[0]}
+                          price={product.price}
+                          discount_price={product.discount_price}
+                          category={product.category}
+                          id={product._id}
+                          countInStock={product.countInStock}
+                          product={product}
+                          averageRating={product.averageRating}
+                        />
+                      ) : null}
+                    </div>
+                  ))}
+                </div>
+              </>
+                )
+            }
+          </>
+        )
+        
       }
     </ExploreLayout>
   )
