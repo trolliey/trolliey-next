c9cb1c4fe32e31aee0b6b8c408e219aa6f70c660
tatendabakozw <tatendabakozw@gmail.com>
2022-06-14 19:39:54 +0200

    fixL ata

--- utils/data.ts
+++ utils/data.ts
@@ -5,24 +5,21 @@ import {
   CurrencyDollarIcon,
 } from '@heroicons/react/outline'
 import sport from '../public/img/sports.svg'
-import books from '../public/img/boooks_and_courses-min.png'
-import boys_fashion from "../public/img/boy's_fashion-min.png"
+import books from '../public/img/books.svg'
 import groceries from '../public/img/groceries.svg'
-import art from '../public/img/arts_and_craft-min.png'
 import gaming from '../public/img/gaming.svg'
 import computers from '../public/img/copmuters.svg'
 import camping from '../public/img/camping.svg'
 import fashion from '../public/img/fashion.svg'
 import homeware from '../public/img/homeware.svg'
 import kitchen from '../public/img/kitchen.svg'
-import baby from '../public/img/baby-min.png'
-import office from '../public/img/office.svg' 
+import baby from '../public/img/baby.svg'
+import office from '../public/img/office.svg'
 import outdoor from '../public/img/outdoor.svg'
-import agric from '../public/img/Agriculture-min.png'
 import tv from '../public/img/tv.svg'
 import cellphone from '../public/img/cellphone.svg'
-import automotive from '../public/img/automotive-min.png'
-import beauty from '../public/img/beaty_and_personal_care-min.png'
+import automotive from '../public/img/automotive.svg'
+import beauty from '../public/img/beauty.svg'
 import bcrypt from 'bcryptjs'
 import promo_1 from '../public/img/promo_1.png'
 import promo_2 from '../public/img/fregrance_sale.png'
@@ -114,7 +111,7 @@ export const data = {
     {
       name: 'Agriculture',
       value: 'agriculture',
-      icon: agric,
+      icon: homeware,
       id: 6,
       sub_categories: [
         { name: 'Seeds and Seedlings', description: '' },
@@ -314,7 +311,7 @@ export const data = {
     {
       name: "Boys' Fashion",
       value: 'boys-fashion',
-      icon: boys_fashion,
+      icon: beauty,
       sub_categories: [
         { name: 'Clothing', description: '' },
         { name: 'Shoes', description: '' },
@@ -347,7 +344,7 @@ export const data = {
     {
       name: 'Arts & Crafts  ',
       value: 'arts-and-rafts',
-      icon: art,
+      icon: automotive,
       sub_categories: [
         { name: 'Painting, Drawing & Art Supplies', description: '' },
         { name: 'Beading & Jewelry Making', description: '' },
