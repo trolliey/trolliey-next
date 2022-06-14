c1ed28d383db1006faa9b17479483296e0e3ecd2
tatendabakozw <tatendabakozw@gmail.com>
2022-06-14 19:45:49 +0200

    fix: category

--- utils/data.ts
+++ utils/data.ts
@@ -11,24 +11,17 @@ import mens_fashion from "../public/img/men's_fashion-min.png"
 import womans_ashion from "../public/img/Women's_Fashion-min.png"
 import computers from '../public/img/Computers-min.png'
 import electronics from '../public/img/Electronics_and_Wearables-min.png'
+import agric from '../public/img/Agriculture-min.png'
+import automotive from '../public/img/automotive-min.png'
 
 import sport from '../public/img/sports.svg'
 import books from '../public/img/books.svg'
-import groceries from '../public/img/groceries.svg'
 import gaming from '../public/img/gaming.svg'
-import camping from '../public/img/camping.svg'
 import fashion from '../public/img/fashion.svg'
-import homeware from '../public/img/homeware.svg'
 import kitchen from '../public/img/kitchen.svg'
 import baby from '../public/img/baby.svg'
-import office from '../public/img/office.svg'
-import outdoor from '../public/img/outdoor.svg'
-import tv from '../public/img/tv.svg'
-import cellphone from '../public/img/cellphone.svg'
-import automotive from '../public/img/automotive.svg'
 import beauty from '../public/img/beauty.svg'
 import bcrypt from 'bcryptjs'
-import promo_1 from '../public/img/promo_1.png'
 import promo_2 from '../public/img/fregrance_sale.png'
 
 export const data = {
@@ -118,7 +111,7 @@ export const data = {
     {
       name: 'Agriculture',
       value: 'agriculture',
-      icon: homeware,
+      icon: agric,
       id: 6,
       sub_categories: [
         { name: 'Seeds and Seedlings', description: '' },
