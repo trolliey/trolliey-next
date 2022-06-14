dee8cdcd07a48200cb54c1ea2ea7efa3a6bcb38a
tatendabakozw <tatendabakozw@gmail.com>
2022-06-14 09:52:36 +0200

    fix: cart z-index

--- components/Sidebars/CartSidebar.tsx
+++ components/Sidebars/CartSidebar.tsx
@@ -40,7 +40,7 @@ function CartSidebar({ open, setOpen }: Props): ReactElement {
 
     return (
         <Transition.Root show={open} as={Fragment}>
-            <Dialog as="div" className="fixed inset-0 overflow-hidden " onClose={setOpen}>
+            <Dialog as="div" className="z-20 fixed inset-0 overflow-hidden " onClose={setOpen}>
                 <div className="absolute inset-0 overflow-hidden">
                     <Transition.Child
                         as={Fragment}
