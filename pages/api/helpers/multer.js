91116c28f1af427e6880c23db229360b97180e43
tatendabakozw <tatendabakozw@gmail.com>
2022-06-15 00:32:39 +0200

    add: uploads folder for multer

--- pages/api/helpers/multer.js
+++ pages/api/helpers/multer.js
@@ -3,7 +3,7 @@ import multer from 'multer'
 // where and how the file will be stored
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './public/uploads')
+    cb(null, './uploads')
   },
   filename: function (req, file, cb) {
     cb(null, new Date().toISOString() + '-' + file.originalname)
