56ff22e77d773670b6e21d50b2918877c038c49b
tatendabakozw <tatendabakozw@gmail.com>
2022-06-14 19:22:08 +0200

    fix:category picture

--- utils/data.ts
+++ utils/data.ts
@@ -5,7 +5,8 @@ import {
   CurrencyDollarIcon,
 } from '@heroicons/react/outline'
 import sport from '../public/img/sports.svg'
-import books from '../public/img/books.svg'
+import books from '../public/img/boooks_and_courses-min.png'
+import boys_fashion from "../public/img/boy's_fashion-min.png"
 import groceries from '../public/img/groceries.svg'
 import art from '../public/img/arts_and_craft-min.png'
 import gaming from '../public/img/gaming.svg'
@@ -14,14 +15,14 @@ import camping from '../public/img/camping.svg'
 import fashion from '../public/img/fashion.svg'
 import homeware from '../public/img/homeware.svg'
 import kitchen from '../public/img/kitchen.svg'
-import baby from '../public/img/baby.svg'
+import baby from '../public/img/baby-min.png'
 import office from '../public/img/office.svg'
 import outdoor from '../public/img/outdoor.svg'
 import agric from '../public/img/Agriculture-min.png'
 import tv from '../public/img/tv.svg'
 import cellphone from '../public/img/cellphone.svg'
 import automotive from '../public/img/automotive-min.png'
-import beauty from '../public/img/beauty.svg'
+import beauty from '../public/img/beaty_and_personal_care-min.png'
 import bcrypt from 'bcryptjs'
 import promo_1 from '../public/img/promo_1.png'
 import promo_2 from '../public/img/fregrance_sale.png'
@@ -313,7 +314,7 @@ export const data = {
     {
       name: "Boys' Fashion",
       value: 'boys-fashion',
-      icon: beauty,
+      icon: boys_fashion,
       sub_categories: [
         { name: 'Clothing', description: '' },
         { name: 'Shoes', description: '' },
