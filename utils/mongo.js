adb6a9d8b59ec9565c886121e4a2a1ecee019bd4
tatendabakozw <tatendabakozw@gmail.com>
2022-06-15 02:45:42 +0200

    fix: newconnection functiuon

--- utils/mongo.js
+++ utils/mongo.js
@@ -2,26 +2,28 @@ import mongoose from 'mongoose'
 
 const connection = {}
 
-async function connect() {
-  if (connection.isConnected) {
-    console.log('already connected')
-    return
-  }
-  else if (mongoose.connections.length > 0) {
-    connection.isConnected = mongoose.connections[0].readyState
-    if (connection.isConnected === 1) {
-      console.log('use previous connection')
-      return
-    }
-    await mongoose.disconnect()
-  }
-  const db = await mongoose.connect(process.env.DATABASE_URL, {
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true,
-    // useCreateIndex: true,
-  })
-  connection.isConnected = db.connections[0].readyState
-}
+// async function connect() {
+//   if (connection.isConnected) {
+//     console.log('already connected')
+//     return
+//   }
+//   else if (mongoose.connections.length > 0) {
+//     connection.isConnected = mongoose.connections[0].readyState
+//     if (connection.isConnected === 1) {
+//       console.log('use previous connection')
+//       return
+//     }
+//     await mongoose.disconnect()
+//   }
+//   const db = await mongoose.connect(process.env.DATABASE_URL, {
+//     // useNewUrlParser: true,
+//     // useUnifiedTopology: true,
+//     // useCreateIndex: true,
+//   })
+//   connection.isConnected = db.connections[0].readyState
+// }
+
+const connect = async () => mongoose.connect(process.env.DATABASE_URL)
 
 async function disconnect() {
   if (connection.isConnected) {
