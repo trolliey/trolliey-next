5a639d50a3928992e9cc3498ebaf4dbafa9fe39c
tatendabakozw <tatendabakozw@gmail.com>
2022-07-30 15:08:01 +0200

    login: error

--- controllers/authController.js
+++ controllers/authController.js
@@ -49,23 +49,62 @@ exports.registerUser = async (req, res) => {
 
 // login user
 exports.loginUser = async (req, res) => {
-  // fields from request
-  const { email, password } = req.body;
+  try {
+    // fields from request
+    const { email, password } = req.body;
 
-  const _user = await User.findOne({ email: email });
+    const _user = await User.findOne({ email: email });
 
-  // user not found
-  if (!_user) {
-    return res.status(404).send({ message: "Account does not exist!" });
-  } else {
-    if (!_user.emailVerified) {
-      return res.status(403).send({ message: "Please verify your email" });
-    }
+    // user not found
+    if (!_user) {
+      return res.status(404).send({ message: "Account does not exist!" });
+    } else {
+      if (!_user.emailVerified) {
+        return res.status(403).send({ message: "Please verify your email" });
+      }
 
-    if (_user.role === "seller") {
+      if (_user.role === "seller") {
+        // decrypt password value from database
+        const store = await Store.findOne({ user: _user._id });
+
+        const password_correct = await bcrypt.compare(password, _user.password);
+        if (password_correct) {
+          const token = await jwt.sign(
+            {
+              name: _user.name,
+              email: _user.email,
+              _id: _user._id,
+              role: _user.role,
+              emailVerified: _user.emailVerified,
+              photoURL: _user.photoURL,
+              store_id: store._id,
+            },
+            process.env.JWT_SECRET
+          );
+          if (token) {
+            const user = {
+              name: _user.name,
+              email: _user.email,
+              _id: _user._id,
+              role: _user.role,
+              emailVerified: _user.emailVerified,
+              photoURL: _user.photoURL,
+              token: token,
+              store_id: store._id,
+            };
+
+            return res.send({ ...user, message: "logged in sucessfully" });
+          } else {
+            return res
+              .status(422)
+              .send({ message: "Failed to login, Wrong details!" });
+          }
+        } else {
+          return res.status(400).send({ message: "Wrong login details" });
+        }
+      }
+
       // decrypt password value from database
-      const store = await Store.findOne({user: _user._id})
-
       const password_correct = await bcrypt.compare(password, _user.password);
       if (password_correct) {
         const token = await jwt.sign(
@@ -76,7 +115,6 @@ exports.loginUser = async (req, res) => {
             role: _user.role,
             emailVerified: _user.emailVerified,
             photoURL: _user.photoURL,
-            store_id: store._id
           },
           process.env.JWT_SECRET
         );
@@ -89,7 +127,6 @@ exports.loginUser = async (req, res) => {
             emailVerified: _user.emailVerified,
             photoURL: _user.photoURL,
             token: token,
-            store_id: store._id
           };
 
           return res.send({ ...user, message: "logged in sucessfully" });
@@ -102,40 +139,7 @@ exports.loginUser = async (req, res) => {
         return res.status(400).send({ message: "Wrong login details" });
       }
     }
-
-    // decrypt password value from database
-    const password_correct = await bcrypt.compare(password, _user.password);
-    if (password_correct) {
-      const token = await jwt.sign(
-        {
-          name: _user.name,
-          email: _user.email,
-          _id: _user._id,
-          role: _user.role,
-          emailVerified: _user.emailVerified,
-          photoURL: _user.photoURL,
-        },
-        process.env.JWT_SECRET
-      );
-      if (token) {
-        const user = {
-          name: _user.name,
-          email: _user.email,
-          _id: _user._id,
-          role: _user.role,
-          emailVerified: _user.emailVerified,
-          photoURL: _user.photoURL,
-          token: token,
-        };
-
-        return res.send({ ...user, message: "logged in sucessfully" });
-      } else {
-        return res
-          .status(422)
-          .send({ message: "Failed to login, Wrong details!" });
-      }
-    } else {
-      return res.status(400).send({ message: "Wrong login details" });
-    }
+  } catch (error) {
+    return res.status(200).send({ message: `${error}` });
   }
 };
