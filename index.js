30d5891d7bf4f979b07c0fd48b232d39db8f11ba
tatendabakozw <tatendabakozw@gmail.com>
2022-10-20 15:33:34 +0200

    fix: verofy ail

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 //applevel middleware
 app.use(helmet());
 app.use(morgan("common"));
-app.use(cors());
+app.use(cors("*"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
