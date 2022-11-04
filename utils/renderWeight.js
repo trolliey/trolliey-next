055e7fdace4d5b951d65e5c44dfa783fe1dc1c73
tatendabakozw <tatendabakozw@gmail.com>
2022-11-04 11:46:56 +0200

    fix: new weights

--- utils/renderWeight.js
+++ utils/renderWeight.js
@@ -1,17 +1,23 @@
 export const renderWeight = (total_weight) => {
   if (total_weight <= 5) {
-    return 10
+    return 7
   }
   if (total_weight > 5 && total_weight <= 10) {
-    return 13
+    return 10
   }
   if (total_weight > 10 && total_weight <= 15) {
-    return 15
+    return 13
   }
   if (total_weight > 15 && total_weight <= 20) {
-    return 30
+    return 25
   }
   if (total_weight > 30 && total_weight <= 50) {
+    return 30
+  }
+  if (total_weight > 50 && total_weight <= 100) {
+    return 35
+  }
+  if (total_weight > 50 && total_weight <= 100) {
     return 40
   }
 }
