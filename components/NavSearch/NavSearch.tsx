0dd274a6f6a3f647e1892072249493daff1c6fbc
tatendabakozw <tatendabakozw@gmail.com>
2022-11-12 14:01:13 +0200

    fix: searching shoer

--- components/NavSearch/NavSearch.tsx
+++ components/NavSearch/NavSearch.tsx
@@ -17,6 +17,7 @@ import { Store } from '../../Context/Store'
 function NavSearch(): ReactElement {
   const { isOpen, onOpen, onClose } = useDisclosure()
   const [search_query, setSearchQuery] = useState<string>('')
+  const [loading, setLoading] = useState(false)
   const history = useRouter()
   const [searched_products, setSearchedProducts] = useState<any>()
   const { dispatch } = useContext(Store)
@@ -35,15 +36,24 @@ function NavSearch(): ReactElement {
   }
 
   useEffect(() => {
+    setLoading(true)
     const getData = async () => {
-      const { data } = await axios.post(
-        `/api/products?page=${1}&keyword=${search_query ? search_query : ''}`,
-        {
-          query: search_query,
-        }
-      )
-      setSearchedProducts(data?.products)
+      try {
+        const { data } = await axios.post(
+          `/api/products?page=${1}&keyword=${search_query ? search_query : ''}`,
+          {
+            query: search_query,
+          }
+        )
+        setSearchedProducts(data?.products)
+        setLoading(false)
+      } catch (error) {
+        setLoading(false)
+        console.log('error on search', error)
+        return
+      }
     }
+    setLoading(false)
     const delayDebounceFn = setTimeout(() => {
       // Send Axios request here
       getData()
@@ -82,73 +92,81 @@ function NavSearch(): ReactElement {
                 <SearchIcon height={20} width={20} className="text-gray-500" />
               </div>
             </div>
-            <div className="border-t border-gray-200 bg-white">
-              {/* <p className="text-center font-semibold capitalize">
-                search results
-              </p> */}
-              {search_query === '' ? (
-                <p className="py-3 text-center text-gray-400">
-                  Nothing has been searched
-                </p>
-              ) : (
-                <div className="flex flex-col">
-                  {searched_products?.length < 1 ? (
-                    <p className="py-3 text-center text-gray-400">
-                      No results found
-                    </p>
-                  ) : (
-                    <>
-                      {searched_products?.map((item: any, index: number) => (
-                        <div
-                          onClick={() => history.push(`/product/d/${item._id}`)}
-                          key={index}
-                          className="flex cursor-pointer flex-row items-center p-2  hover:bg-gray-100"
-                        >
-                          {/* <Avatar src={item.picture}   rounded={"md"} /> */}
-                          <div className="flex">
-                            <div className="relative h-16 w-16 overflow-hidden rounded bg-gray-50">
-                              <Image
-                                src={item.pictures[0]}
-                                layout="fill"
-                                objectFit="contain"
-                              />
+            {loading ? (
+               <p className="py-3 text-center text-gray-400">
+               Searching ...
+             </p>
+            ) : (
+              <div className="border-t border-gray-200 bg-white">
+                {/* <p className="text-center font-semibold capitalize">
+              search results
+            </p> */}
+                {search_query === '' ? (
+                  <p className="py-3 text-center text-gray-400">
+                    Nothing has been searched
+                  </p>
+                ) : (
+                  <div className="flex flex-col">
+                    {searched_products?.length < 1 ? (
+                      <p className="py-3 text-center text-gray-400">
+                        No results found
+                      </p>
+                    ) : (
+                      <>
+                        {searched_products?.map((item: any, index: number) => (
+                          <div
+                            onClick={() =>
+                              history.push(`/product/d/${item._id}`)
+                            }
+                            key={index}
+                            className="flex cursor-pointer flex-row items-center p-2  hover:bg-gray-100"
+                          >
+                            {/* <Avatar src={item.picture}   rounded={"md"} /> */}
+                            <div className="flex">
+                              <div className="relative h-16 w-16 overflow-hidden rounded bg-gray-50">
+                                <Image
+                                  src={item.pictures[0]}
+                                  layout="fill"
+                                  objectFit="contain"
+                                />
+                              </div>
+                            </div>
+                            <div className="flex flex-col pl-4">
+                              <Text
+                                noOfLines={1}
+                                className="font-semibold text-gray-800"
+                              >
+                                {item.title}
+                              </Text>
+                              <Text
+                                noOfLines={1}
+                                className="text-sm text-gray-400"
+                              >
+                                {item.category}
+                              </Text>
+                              <Text
+                                noOfLines={1}
+                                className="text-sm text-gray-400"
+                              >
+                                {item.price} {item.currency_type}
+                              </Text>
                             </div>
                           </div>
-                          <div className="flex flex-col pl-4">
-                            <Text
-                              noOfLines={1}
-                              className="font-semibold text-gray-800"
-                            >
-                              {item.title}
-                            </Text>
-                            <Text
-                              noOfLines={1}
-                              className="text-sm text-gray-400"
-                            >
-                              {item.category}
-                            </Text>
-                            <Text
-                              noOfLines={1}
-                              className="text-sm text-gray-400"
-                            >
-                              {item.price} {item.currency_type}
-                            </Text>
-                          </div>
-                        </div>
-                      ))}
-                      <div className="my-1"></div>
-                      {searched_products?.length > 8 && (
-                        <Link href={'/eplore'}>
-                          <a className="pt-2capitalize flex cursor-pointer flex-col border-t border-gray-300 text-center text-gray-500">
-                            View all results
-                          </a>
-                        </Link>
-                      )}
-                    </>
-                  )}
-                </div>
-              )}
-            </div>
+                        ))}
+                        <div className="my-1"></div>
+                        {searched_products?.length > 8 && (
+                          <Link href={'/eplore'}>
+                            <a className="pt-2capitalize flex cursor-pointer flex-col border-t border-gray-300 text-center text-gray-500">
+                              View all results
+                            </a>
+                          </Link>
+                        )}
+                      </>
+                    )}
+                  </div>
+                )}
+              </div>
+            )}
           </ModalBody>
         </ModalContent>
       </Modal>
