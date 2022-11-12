d96c28892e4bb1c9a3f16bbb0a656788b5981ff3
tatendabakozw <tatendabakozw@gmail.com>
2022-11-12 13:48:24 +0200

    fix: nre url for login

--- utils/apiUrl.js
+++ utils/apiUrl.js
@@ -1,5 +1,5 @@
-// export const apiUrl = `https://trolliey-backend.herokuapp.com`
-// export const socketUrl = `https://trolliey-backend.herokuapp.com`
+export const apiUrl = `https://trolliey-backend.herokuapp.com`
+export const socketUrl = `https://trolliey-backend.herokuapp.com`
 
-export const apiUrl = `http://localhost:5000`
-export const socketUrl = `http://localhost:5500`
+// export const apiUrl = `http://localhost:5000`
+// export const socketUrl = `http://localhost:5500`
